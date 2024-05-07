fb4824c700f136725695ef803f196715db9e572b
jiabaoliu101 <166598325+jiabaoliu101@users.noreply.github.com>
2024-05-07 19:38:50 +0800

    环境搭建
    前端+后端springboot

---
+++ IELTS/IELTS/src/main/java/com/example/ielts/IeltsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ielts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeltsApplication.class, args);
+    }
+
+}
